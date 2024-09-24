4663e7bc394e1b4e5d9f2f3cf2cfe68776e10d38
yu256 <122450013+yu256@users.noreply.github.com>
2024-09-24 15:19:08 +0900

    キー入力を実装

---
+++ src/main/scala/app/Game/View.scala
@@ -0,0 +1,37 @@
+package app.Game
+
+import com.raquo.laminar.api.L.*
+
+def gameView =
+  import RomajiParser.*
+  val japanese = Var("")
+  val input = Var("")
+
+  div(
+      cls := "h-screen w-screen flex flex-col justify-center items-center",
+      tabIndex := 0,
+      autoFocus := true,
+      onKeyDown --> (_.key match {
+        case key if key.length == 1 =>
+          input.update(_ + key)
+          parseRomaji(input.now()) match {
+            case RomajiState.Complete(jp) =>
+              japanese.update(_ + jp)
+              input.set("")
+            case RomajiState.Invalid => input.set("")
+            case RomajiState.Partial(jp, rest) =>
+              japanese.update(_ + jp)
+              input.set(rest)
+            case RomajiState.Progress =>
+          }
+        case "Backspace" | "Delete" =>
+          (if input.now().nonEmpty then input else japanese)
+            .update(_.dropRight(1))
+      }),
+      div(
+          cls := "flex flex-col items-center",
+          span(
+              text <-- japanese.signal.combineWith(input.signal).map(_ ++ _)
+          )
+      )
+  )
--- src/main/scala/app/main.scala
+++ src/main/scala/app/main.scala
@@ -1,6 +1,8 @@
 package app
 
+import Game.gameView
 import com.raquo.laminar.api.L.*
+import components.*
 import org.scalajs.dom
 
 @main def main(): Unit =
@@ -8,4 +10,27 @@ import org.scalajs.dom
   renderOnDomContentLoaded(appContainer, app)
 
 lazy val app =
-  div()
+  val isStarted = Var(false)
+
+  span(
+      child <-- isStarted.signal.map {
+        if _ then gameView else startScreen(_ => isStarted.set(true))
+      }
+  )
+
+def startScreen(onClickStart: dom.MouseEvent => Unit) =
+  div(
+      cls := "flex flex-col items-center justify-center h-screen bg-gray-800",
+      h1(
+          cls := "text-5xl font-bold mb-6 text-center text-white",
+          "AtsusugiDa"
+      ),
+      p(
+          cls := "text-lg text-center text-gray-400 mb-8",
+          "下のボタンを押してスタート"
+      ),
+      myButton(
+          "Start Game",
+          onClickStart
+      )
+  )
